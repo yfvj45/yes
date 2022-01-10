7ef99651f37f5cbc9048370711aa868ed462fabe
yfvj45 <97280807+yfvj45@users.noreply.github.com>
2022-01-10 11:55:18 +0530

    Update try.py

--- try.py
+++ try.py
@@ -9,7 +9,7 @@ from selenium import webdriver
 options = uc.ChromeOptions()
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
 #options.add_argument('--user-agent=%s' % USER_AGENT)
-options.add_argument('--headless')
+#options.add_argument('--headless')
 options.add_argument("--no-sandbox"); # Bypass OS security model
 
 driver = uc.Chrome(options=options,version_main=96)
