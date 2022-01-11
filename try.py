383b4102e9ada5c8419dd86848be18ae98279297
yfvj45 <97280807+yfvj45@users.noreply.github.com>
2022-01-11 14:42:34 +0530

    Update try.py

--- try.py
+++ try.py
@@ -7,64 +7,42 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
-options = uc.ChromeOptions()
-#USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
-#options.add_argument('--user-agent=%s' % USER_AGENT)
-options.headless=True
-options.add_argument('--headless')
+def create_chromedriver(title,t):
+    options = uc.ChromeOptions()
+    options.headless=True
+    options.add_argument('--headless')
+    options.set_capability("detach", True)
+    driver = uc.Chrome(options=options,version_main=96)
+    driver.get("https://temp-mail.org/en/")
+    print("Start")
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get("https://www.youtube.com/")
+    driver.maximize_window()    
+    time.sleep(20)
+    driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
+    driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(title)
+    driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
+    time.sleep(5)
+    driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]').click()
+    time.sleep(3)
+    driver.save_screenshot('Video.png')
+    print("Video_found")
 
-driver = uc.Chrome(options=options,version_main=96)
+     
+  
 
-driver.get("https://temp-mail.org/en/")
-print("Start")
-driver.execute_script("window.open('');")
-time.sleep(2)
-driver.switch_to.window(driver.window_handles[1])
-driver.get("https://www.youtube.com/")
-driver.maximize_window()    
-time.sleep(20)
-driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
-driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys("USE  unlimited internet by bypass Wifi login  break the limit.")
-driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
-time.sleep(5)
-
-driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]').click()
-print("Started")
-time.sleep(3)
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[2])
-driver.get("https://www.youtube.com/watch?v=KhgjRa0wli4&list=UUOsxh5uNZFWSC7aYR-vvm-A")
-time.sleep(2)
-driver.save_screenshot('ss.png')
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[3])
-driver.get("https://youtu.be/6G-pTxdjao4")
-time.sleep(2)
-driver.execute_script("window.open('');")    
-driver.switch_to.window(driver.window_handles[4])
-driver.get("https://youtu.be/eJeBFr4FgDI")
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[5])
-driver.get("https://youtu.be/1V-tQzN6Azw")
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[6])
-driver.get("https://youtu.be/-kSxVQghbY0")
-time.sleep(2)
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[7])
-driver.get("https://youtu.be/h2889QVThX4")
-time.sleep(2)
-time.sleep(600)
-print("End")
-
+proxies  = []
+with open('p.txt') as f:
+    for line in f:
+        proxies.append(line)
+for i in proxies:
+    s=i.split(':')
+    title=s[0]
+    t=int(s[1].strip())
+    print(title,t)
+    create_chromedriver(title,t)
     
-    
-    
-    
-    
-
-
-
 
     
     
