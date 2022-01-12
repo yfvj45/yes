4f09c76b4cb92ceb910d2584d42670b580c700ca
yfvj45 <97280807+yfvj45@users.noreply.github.com>
2022-01-12 21:18:27 +0530

    Update try.py

--- try.py
+++ try.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-import random
+
 
 def create_chromedriver(title,t):
     options = uc.ChromeOptions()
@@ -20,13 +20,13 @@ def create_chromedriver(title,t):
     driver.switch_to.window(driver.window_handles[1])
     driver.get("https://www.youtube.com/")
     driver.maximize_window()
-    k=random.randint(15,25)
-    time.sleep(k)
+    
+    time.sleep(20)
     driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').click()
     driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(title)
     driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
-    f=random.randint(0,5)
-    time.sleep(f)
+    
+    time.sleep(3)
     driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]').click()
     time.sleep(3)
     driver.save_screenshot('Video.png')
